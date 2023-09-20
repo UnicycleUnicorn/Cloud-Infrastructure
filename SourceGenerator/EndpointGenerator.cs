cae65c7bef58f86a724bda0b834b75ac7d076430
UnicycleUnicorn <brand227796@gmail.com>
2023-09-20 11:24:31 -0400

    Add project files.

---
+++ SourceGenerator/EndpointGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Server.Generators
+{
+
+    [Generator]
+    public class EndpointGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource($"test.generated.cs", "// auto generated");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
